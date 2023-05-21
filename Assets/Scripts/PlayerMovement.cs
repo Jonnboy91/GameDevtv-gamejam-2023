e34b0e57b27efa7bdab935ccc0ec3dba28bce869
Jonnboy91 <jon.nesten@gmail.com>
2023-05-21 19:08:31 +0300

    Basic movement input

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+
+    float gravityScaleAtStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void OnMove(InputValue value){
+        moveInput = value.Get<Vector2>();
+    }
+
+
+    void OnJump(InputValue value){
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+            return;
+        }
+
+        if(value.isPressed){
+            myRigidBody.velocity += new Vector2 (0f, jumpSpeed);
+        }
+    }
+
+    void Move(){
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+
+}
