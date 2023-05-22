e5a80c0596f6af08860806d4add2ff1faffe87c9
Jonnboy91 <jon.nesten@gmail.com>
2023-05-22 15:57:51 +0300

    Very basic start and quit button. Still needs to be designed and set up to look nicer, but for now to test the flow this will do

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Topdown level 1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
