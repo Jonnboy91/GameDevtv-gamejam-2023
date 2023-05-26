38cb72f72a90f8e32211782fdc94947009f43d8c
Jonnboy91 <jon.nesten@gmail.com>
2023-05-26 18:26:24 +0300

    Added new skin and added animations, edited popeye to only double the transform.scale)

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -12,15 +12,7 @@ public class PlayerAnimations : MonoBehaviour
 
     public void ChangeState(Vector2 value)
     {
-        if(value.x != 0 || value.y != 0){
-            _animator.SetBool("isMoving", true);
             _animator.SetFloat("X Float", value.x);
             _animator.SetFloat("Y Float", value.y);
-        }else{
-            _animator.SetBool("isMoving", false);
-            _animator.SetFloat("X Float", value.x);
-            _animator.SetFloat("Y Float", value.y);
-        }
-
     }
 }
