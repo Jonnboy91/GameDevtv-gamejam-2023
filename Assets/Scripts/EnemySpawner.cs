ee54b24e572807ca20d3606a020e11e03003214f
Jonnboy91 <jon.nesten@gmail.com>
2023-05-29 14:57:52 +0300

    EnemySpawner now gradually over certain period of time is spawning faster until it hits the final spawn rate.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,18 +8,21 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] List<GameObject> spawners;
     [SerializeField] float startSpawnInterval = 1.5f;
     [SerializeField] float spawnInterval = 0f;
-    [SerializeField] float finalSpawnInterval = 0.6f; 
+    [SerializeField] float finalSpawnInterval = 0.5f; 
     [SerializeField] float spawnIntervalChangeTime = 2f;
     [SerializeField] int maxEnemies = 125;
     [SerializeField] float startDelay = 0.5f; // Can't be 0, since it might try to spawn before getting NavMesh
 
     private int currentEnemies = 0;
 
+    private Coroutine spawnCoroutine;
+
     private void Start()
     {
         spawnInterval = startSpawnInterval;
         InvokeRepeating(nameof(SpawnEnemy), startDelay, spawnInterval);
-        StartCoroutine(GraduallyIncreaseSpawnRate());
+        if(spawnCoroutine == null) 
+        spawnCoroutine = StartCoroutine(GraduallyIncreaseSpawnRate());
     }
 
     private void SpawnEnemy()
@@ -46,13 +49,25 @@ public class EnemySpawner : MonoBehaviour
         while (spawnInterval > finalSpawnInterval)
         {
             spawnInterval -= 0.1f;
+            UpdateSpawnRate();
             yield return new WaitForSeconds(spawnIntervalChangeTime);
         }
         spawnInterval = finalSpawnInterval;
-
+        UpdateSpawnRate();
+        StopCoroutine(spawnCoroutine);
+        spawnCoroutine = null;
     }
+
+    private void UpdateSpawnRate()
+    {
+        CancelInvoke(nameof(SpawnEnemy));
+        InvokeRepeating(nameof(SpawnEnemy), 0, spawnInterval);
+    }
+
+
     public void IncreaseSpawnRate(){
         spawnInterval *= 0.8f;
+        UpdateSpawnRate();
     }
 
     public void EnemyDestroyed()
